c564e75d8d0c90e46cccd3a4236e280bc8855eb5
David Whiting <dwhiting06@gmail.com>
2015-02-02 16:57:36 -0500

    Added checkMove() and switchPlayer() functions
    1) checkMove() takes a move as an input, and checks to see if that move
    is valid.
    2) added self.player to distinguish player X from player O.
    3) switchPlayer() switches the player from X to O and vise-versa.
    4) Misc. changes to coding style. This style will be maintained from now
    on.

--- Board.py
+++ Board.py
@@ -1,28 +1,46 @@
-
 class Board:
     
     def __init__(self):
-        self.gameOver = False
-        self.winner = "Nobody"
-        self.moves = 0
-        self.board = [  [" ", " ", " ",], 
-                        [" ", " ", " ",],
-                        [" ", " ", " " ]  ]
+        self.gameOver = False;
+        self.winner = "Nobody";
+        self.moves = 0;
+        self.player = 'X';
+        self.board = [[" ", " ", " ",], 
+                      [" ", " ", " ",],
+                      [" ", " ", " " ]];
 
     def checkGameOver(self):
-        if self.moves < 5: # Nobody can win until they place > 3 pieces
-            return
-        for x in range(0,3): # Checking for a winner
-            if self.board[x][0] == self.board[x][1] == self.board[x][2]: # Row
-                self.gameOver = True
-                self.winner = self.board[x][0]
-                return
-            if self.board[0][x] == self.board[1][x] == self.board[2][x]: # Column
-                self.gameOver = True
-                self.winner = self.board[0][x]
-                return
-        if self.moves == 9: # Game is over, no winners found
-            self.gameOver = True
-            return
+        if (self.moves < 5): # Impossible to win under 5 moves
+            return;
+        for x in range(0,3): # Check for winner
+            if (self.board[x][0] == self.board[x][1] == self.board[x][2]): # Row
+                self.gameOver = True;
+                self.winner = self.board[x][0];
+                return;
+            if (self.board[0][x] == self.board[1][x] == self.board[2][x]): # Column
+                self.gameOver = True;
+                self.winner = self.board[0][x];
+                return;
+        if (self.moves == 9): # Maximum moves
+            self.gameOver = True;
+            return;
 
-    
+    def checkMove(self, move): # Checks for correct move E.g. A2, c1, B3, etc.
+        if (not isinstance(move, str)):
+            return False;
+        elif (len(move) != 2):
+            return False;
+        elif (move[0] not in ["a", "A", "b", "B", "c", "C"]):
+            return False;
+        elif (move[1] not in ["1", "2", "3"]):
+            return False;
+        else:
+            return True;
+       
+    def switchPlayer(self):
+        if (self.player == 'X'):
+            self.player = 'O';
+            return;
+        else:
+            self.player = 'X';
+            return;
