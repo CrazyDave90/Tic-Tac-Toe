10a912769079fcab14e866b324582c9d3ee1ff67
David Whiting <dwhiting06@gmail.com>
2015-02-02 14:52:21 -0500

    Created Board class
    Board class handles the creation and management of tic-tac-toe boards.

---
+++ Board.py
@@ -0,0 +1,9 @@
+
+class Board:
+    
+    def __init__(self):
+        self.gameOver = False
+        self.moves = 0
+        self.board = [ " ", " ", " ",
+                       " ", " ", " ",
+                       " ", " ", " " ]
