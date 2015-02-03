bd7b88b636881f70e5c3b1a9aac673a4806ae980
David Whiting <dwhiting06@gmail.com>
2015-02-03 14:05:23 -0500

    Refactored checkGameOver()
    1) checkGameOver() has been shorted to take the input of all 8 possible
    winning combinations to declare a winner. It now sets self.winner to
    self.player, and only if the winning row is the same as the current
    player. This is to avoid a game over for a 3 blank spaces.
    2) Simple test cases added.

--- Board.py
+++ Board.py
@@ -12,25 +12,26 @@ class Board:
             [" ", " ", " ",],
             [" ", " ", " " ]
         ]
+        self.winCombos = (
+            ((0,0), (0,1), (0,2)),
+            ((1,0), (1,1), (1,2)),
+            ((2,0), (2,1), (2,2)),
+            ((0,0), (1,0), (2,0)),
+            ((0,1), (1,1), (2,1)),
+            ((0,2), (1,2), (2,2)),
+            ((0,0), (1,1), (2,2)),
+            ((0,2), (1,1), (2,0))
+        )
 
 
     def checkGameOver(self):
-        for x in range(0,3):
-            if  (self.board[x][0] == self.board[x][1] == self.board[x][2])
-            and (self.board[x][0] != " "):
-                self.gameOver = True
-                self.winner = self.board[x][0]
-                return
-            if  (self.board[0][x] == self.board[1][x] == self.board[2][x])
-            and (self.board[0][x] != " "):
-                self.gameOver = True
-                self.winner = self.board[0][x]
-                return
-            if  (self.board[0][0] == self.board[1][1] == self.board[2][2] or
-                 self.board[2][0] == self.board[1][1] == self.board[0][2])
-            and (self.board[1][1] != " "):
-                self.gameOver = True
+        for combo in self.winCombos:
+            space1 = self.board[combo[0][0]][combo[0][1]]
+            space2 = self.board[combo[1][0]][combo[1][1]]
+            space3 = self.board[combo[2][0]][combo[2][1]]
+            if space1 == space2 == space3 == self.player:
                 self.winner = self.player
+                self.gameOver = True
                 return
         if (self.moves == 9):
             self.gameOver = True
@@ -55,3 +56,34 @@ class Board:
             return
         self.player = 'X'
         return
+
+
+#game = Board()
+#game.moves = 9
+#game.checkGameOver()
+#print (game.gameOver)
+#print ("expected: True")
+#print (game.winner)
+#print ("expected: None")
+#game.moves = 5
+#game.board = [
+#        ["X", "X", "X",], 
+#        ["X", " ", " ",],
+#        ["X", " ", " " ]
+#]
+#game.checkGameOver()
+#print (game.gameOver)
+#print ("expected: True")
+#print (game.winner)
+#print ("expected: X")
+#game.board = [
+#        ["O", " ", " ",], 
+#        ["O", " ", " ",],
+#        ["O", " ", " " ]
+#]
+#game.switchPlayer()
+#game.checkGameOver()
+#print (game.gameOver)
+#print ("expected: True")
+#print (game.winner)
+#print ("expected: O")
