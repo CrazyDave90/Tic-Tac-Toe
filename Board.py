89223fffa6cc74e9b6e4c96a6e70cbf365af7e3e
David Whiting <dwhiting06@gmail.com>
2015-02-03 00:02:11 -0500

    Syntax Editing and Fixing checkGameOver() bug.
    1) Fixed checkGameOver() to not set self.winner to a 3 in a row on blank
    spots
    2) Changed checkMove() to checkValidMove()
    3) Misc. syntax adjustments to make code easier to read

--- Board.py
+++ Board.py
@@ -1,46 +1,57 @@
 class Board:
+
     
     def __init__(self):
-        self.gameOver = False;
-        self.winner = "Nobody";
-        self.moves = 0;
-        self.player = 'X';
-        self.board = [[" ", " ", " ",], 
-                      [" ", " ", " ",],
-                      [" ", " ", " " ]];
+        self.gameOver = False
+        self.winner = None
+        self.numMoves = 0
+        self.player = 'X'
+        self.move = ""
+        self.board = [
+            [" ", " ", " ",], 
+            [" ", " ", " ",],
+            [" ", " ", " " ]
+        ]
+
 
     def checkGameOver(self):
-        if (self.moves < 5): # Impossible to win under 5 moves
-            return;
-        for x in range(0,3): # Check for winner
-            if (self.board[x][0] == self.board[x][1] == self.board[x][2]): # Row
-                self.gameOver = True;
-                self.winner = self.board[x][0];
-                return;
-            if (self.board[0][x] == self.board[1][x] == self.board[2][x]): # Column
-                self.gameOver = True;
-                self.winner = self.board[0][x];
-                return;
-        if (self.moves == 9): # Maximum moves
-            self.gameOver = True;
-            return;
+        for x in range(0,3):
+            if  (self.board[x][0] == self.board[x][1] == self.board[x][2])
+            and (self.board[x][0] != " "):
+                self.gameOver = True
+                self.winner = self.board[x][0]
+                return
+            if  (self.board[0][x] == self.board[1][x] == self.board[2][x])
+            and (self.board[0][x] != " "):
+                self.gameOver = True
+                self.winner = self.board[0][x]
+                return
+            if  (self.board[0][0] == self.board[1][1] == self.board[2][2] or
+                 self.board[2][0] == self.board[1][1] == self.board[0][2])
+            and (self.board[1][1] != " "):
+                self.gameOver = True
+                self.winner = self.player
+                return
+        if (self.moves == 9):
+            self.gameOver = True
+        return
 
-    def checkMove(self, move): # Checks for correct move E.g. A2, c1, B3, etc.
+
+    def checkValidMove(self, move):
         if (not isinstance(move, str)):
-            return False;
+            return False
         elif (len(move) != 2):
-            return False;
-        elif (move[0] not in ["a", "A", "b", "B", "c", "C"]):
-            return False;
-        elif (move[1] not in ["1", "2", "3"]):
-            return False;
-        else:
-            return True;
+            return False
+        elif (move[0] not in "ABC"):
+            return False
+        elif (move[1] not in "123"):
+            return False
+        return True
+
        
     def switchPlayer(self):
         if (self.player == 'X'):
-            self.player = 'O';
-            return;
-        else:
-            self.player = 'X';
-            return;
+            self.player = 'O'
+            return
+        self.player = 'X'
+        return
