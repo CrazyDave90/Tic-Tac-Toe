c8be265d9cde88d8c7f8a29e69467928c3c9ddfd
David Whiting <dwhiting06@gmail.com>
2015-02-03 23:25:36 -0500

    Renaming Variables + Comments
    1) Renamed self.player to self.currentPlayer.
    2) Removed unused self.move variable.
    3) Fixed a bug in checkGameOver() regarding the self.numMoves variable.
    4) Misc comments in self.winCombos, checkGameOver(), and
    checkValidMove(move) to improve readability.

--- Board.py
+++ Board.py
@@ -5,40 +5,42 @@ class Board:
         self.gameOver = False
         self.winner = None
         self.numMoves = 0
-        self.player = 'X'
-        self.move = ""
+        self.currentPlayer = 'X'
         self.board = [
             [" ", " ", " ",], 
             [" ", " ", " ",],
             [" ", " ", " " ]
         ]
         self.winCombos = (
+            # Horizontal
             ((0,0), (0,1), (0,2)),
             ((1,0), (1,1), (1,2)),
             ((2,0), (2,1), (2,2)),
+            # Vertical
             ((0,0), (1,0), (2,0)),
             ((0,1), (1,1), (2,1)),
             ((0,2), (1,2), (2,2)),
+            #Diagonal
             ((0,0), (1,1), (2,2)),
             ((0,2), (1,1), (2,0))
         )
 
 
     def checkGameOver(self):
-        for combo in self.winCombos:
+        for combo in self.winCombos: # Possible winning combinations
             space1 = self.board[combo[0][0]][combo[0][1]]
             space2 = self.board[combo[1][0]][combo[1][1]]
             space3 = self.board[combo[2][0]][combo[2][1]]
-            if space1 == space2 == space3 == self.player:
-                self.winner = self.player
+            if space1 == space2 == space3 == self.currentPlayer: # Avoid GameOver with 3 blank spaces
+                self.winner = self.currentPlayer
                 self.gameOver = True
                 return
-        if (self.moves == 9):
+        if (self.numMoves == 9): # All spaces filled
             self.gameOver = True
         return
 
 
-    def checkValidMove(self, move):
+    def checkValidMove(self, move): # E.g. A1, B3, C2
         if (not isinstance(move, str)):
             return False
         elif (len(move) != 2):
@@ -51,21 +53,21 @@ class Board:
 
        
     def switchPlayer(self):
-        if (self.player == 'X'):
-            self.player = 'O'
+        if (self.currentPlayer == 'X'):
+            self.currentPlayer = 'O'
             return
-        self.player = 'X'
+        self.currentPlayer = 'X'
         return
 
-
+    
 #game = Board()
-#game.moves = 9
+#game.numMoves = 9
 #game.checkGameOver()
 #print (game.gameOver)
 #print ("expected: True")
 #print (game.winner)
 #print ("expected: None")
-#game.moves = 5
+#game.numMoves = 5
 #game.board = [
 #        ["X", "X", "X",], 
 #        ["X", " ", " ",],
